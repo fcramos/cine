36c5d8d65faeb95c601e19b6b2c9bc779dc52beb
Felipe Corrêa Ramos <lype086@gmail.com>
2015-06-20 12:55:15 -0300

    Utilizando o Django-Grappelli na página admin

--- cine/urls.py
+++ cine/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cine.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
